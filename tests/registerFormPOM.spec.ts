cb29adc55410052c291967bf84f8a47105327a7c
Iryna Blazhenko <iryna.blazhenko@gmail.com>
2024-05-07 13:47:46 +0300

    added await

--- tests/registerFormPOM.spec.ts
+++ tests/registerFormPOM.spec.ts
@@ -12,7 +12,7 @@ test.describe('Positive scenario. User can be registered', () => {
     //go to the Register form
     test.beforeEach(async ({ page }) => {
         signUpForm = new SignUpForm(page);
-        signUpForm.open();
+        await signUpForm.open();
     });
 
     //delete registered account
@@ -33,7 +33,7 @@ test.describe('Field Name validation', () => {
     //go to the Register form
     test.beforeEach(async ({ page }) => {
         signUpForm = new SignUpForm(page);
-        signUpForm.open();
+        await signUpForm.open();
     });
 
     test.afterEach(async ({ page }) => {
@@ -43,27 +43,27 @@ test.describe('Field Name validation', () => {
 
     test('Name is required', async ({ page }) => {
         const errorMessage = 'Name required';
-        signUpForm.fieldValidation(signUpForm.nameField, '', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.nameField, '', errorMessage);
     });
 
     test('Name should be greater than 1', async ({ page }) => {
         const errorMessage = 'Name has to be from 2 to 20 characters long';
-        signUpForm.fieldValidation(signUpForm.nameField, 't', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.nameField, 't', errorMessage);
     });
 
     test('Name should be less than 20', async ({ page }) => {
         const errorMessage = 'Name has to be from 2 to 20 characters long';
-        signUpForm.fieldValidation(signUpForm.nameField, 'qwertyuiopasdfghjklzx', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.nameField, 'qwertyuiopasdfghjklzx', errorMessage);
     });
 
     test('Name shouldn"t contain space', async ({ page }) => {
         const errorMessage = 'Name is invalid';
-        signUpForm.fieldValidation(signUpForm.nameField, 'some name', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.nameField, 'some name', errorMessage);
     });
 
     test('Name should be only English alphabet', async ({ page }) => {
         const errorMessage = 'Name is invalid';
-        signUpForm.fieldValidation(signUpForm.nameField, 'назва', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.nameField, 'назва', errorMessage);
     });
 });
 
@@ -73,7 +73,7 @@ test.describe('Field Last name validation', () => {
     //go to the Register form
     test.beforeEach(async ({ page }) => {
         signUpForm = new SignUpForm(page);
-        signUpForm.open();
+        await signUpForm.open();
         await signUpForm.nameField.fill('Test');
     });
     test.afterEach(async ({ page }) => {
@@ -83,27 +83,27 @@ test.describe('Field Last name validation', () => {
 
     test('Last name is required', async ({ page }) => {
         const errorMessage = 'Last name required';
-        signUpForm.fieldValidation(signUpForm.lastNameField, '', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.lastNameField, '', errorMessage);
     });
 
     test('Last name should be greater than 1', async ({ page }) => {
         const errorMessage = 'Last name has to be from 2 to 20 characters long';
-        signUpForm.fieldValidation(signUpForm.lastNameField, 't', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.lastNameField, 't', errorMessage);
     });
 
     test('Last name should be less than 21', async ({ page }) => {
         const errorMessage = 'Last name has to be from 2 to 20 characters long';
-        signUpForm.fieldValidation(signUpForm.lastNameField, 'qwertyuiopasdfghjklzx', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.lastNameField, 'qwertyuiopasdfghjklzx', errorMessage);
     });
 
     test('Last name shouldn"t contain space', async ({ page }) => {
         const errorMessage = 'Last name is invalid';
-        signUpForm.fieldValidation(signUpForm.lastNameField, 'some last name', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.lastNameField, 'some last name', errorMessage);
     });
 
     test('Last name should be only English alphabet', async ({ page }) => {
         const errorMessage = 'Last name is invalid';
-        signUpForm.fieldValidation(signUpForm.lastNameField, 'фамілія', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.lastNameField, 'фамілія', errorMessage);
     });
 });
 
@@ -113,7 +113,7 @@ test.describe('Field Email validation', () => {
     //go to the Register form
     test.beforeEach(async ({ page }) => {
         signUpForm = new SignUpForm(page);
-        signUpForm.open();
+        await signUpForm.open();
         await signUpForm.nameField.fill('Name');
         await signUpForm.lastNameField.fill('LastName');
     });
@@ -127,19 +127,19 @@ test.describe('Field Email validation', () => {
 
     test('Email is required', async ({ page }) => {
         const errorMessage = 'Email required';
-        signUpForm.fieldValidation(signUpForm.emailField, '', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.emailField, '', errorMessage);
     });
 
     test('Email should contain @', async ({ page }) => {
-        signUpForm.fieldValidation(signUpForm.emailField, 'test.test.test', invalidEmail);
+        await signUpForm.fieldValidation(signUpForm.emailField, 'test.test.test', invalidEmail);
     });
 
     test('Email shouldn"t contain space', async ({ page }) => {
-        signUpForm.fieldValidation(signUpForm.emailField, 'some name@gmail.com', invalidEmail);
+        await signUpForm.fieldValidation(signUpForm.emailField, 'some name@gmail.com', invalidEmail);
     });
 
     test('Email should contain a few symbols and a dot after @', async ({ page }) => {
-        signUpForm.fieldValidation(signUpForm.emailField, 'name@gmailcom', invalidEmail);
+        await signUpForm.fieldValidation(signUpForm.emailField, 'name@gmailcom', invalidEmail);
     });
 });
 
@@ -149,7 +149,7 @@ test.describe('Field Password validation', () => {
     //go to the Register form
     test.beforeEach(async ({ page }) => {
         signUpForm = new SignUpForm(page);
-        signUpForm.open();
+        await signUpForm.open();
         await signUpForm.nameField.fill('Name');
         await signUpForm.lastNameField.fill('LastName');
         await signUpForm.emailField.fill(randomEmail);
@@ -164,27 +164,27 @@ test.describe('Field Password validation', () => {
 
     test('Password is required', async ({ page }) => {
         const errorMessage = 'Password required';
-        signUpForm.fieldValidation(signUpForm.passwordField, '', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.passwordField, '', errorMessage);
     });
 
     test('Password should be greater than 7', async ({ page }) => {
-        signUpForm.fieldValidation(signUpForm.passwordField, 'Passw1@', invalidPassword);
+        await signUpForm.fieldValidation(signUpForm.passwordField, 'Passw1@', invalidPassword);
     });
 
     test('Password should be less than 16', async ({ page }) => {
-        signUpForm.fieldValidation(signUpForm.passwordField, 'P a s s w o rd1@', invalidPassword);
+        await signUpForm.fieldValidation(signUpForm.passwordField, 'P a s s w o rd1@', invalidPassword);
     });
 
     test('Password should contain minimun 1 integer', async ({ page }) => {
-        signUpForm.fieldValidation(signUpForm.passwordField, 'Password@', invalidPassword);
+        await signUpForm.fieldValidation(signUpForm.passwordField, 'Password@', invalidPassword);
     });
 
     test('Password should contain minimum 1 capital letter', async ({ page }) => {
-        signUpForm.fieldValidation(signUpForm.passwordField, 'password1@', invalidPassword);
+        await signUpForm.fieldValidation(signUpForm.passwordField, 'password1@', invalidPassword);
     });
 
     test('Password should contain minimum 1 small letter', async ({ page }) => {
-        signUpForm.fieldValidation(signUpForm.passwordField, 'PASSWORD1@', invalidPassword);
+        await signUpForm.fieldValidation(signUpForm.passwordField, 'PASSWORD1@', invalidPassword);
     });
 });
 
@@ -194,7 +194,7 @@ test.describe('Field Re-enter password validation', () => {
     //go to the Register form
     test.beforeEach(async ({ page }) => {
         signUpForm = new SignUpForm(page);
-        signUpForm.open();
+        await signUpForm.open();
         await signUpForm.nameField.fill('Name');
         await signUpForm.lastNameField.fill('LastName');
         await signUpForm.emailField.fill(randomEmail);
@@ -208,12 +208,12 @@ test.describe('Field Re-enter password validation', () => {
 
     test('Re-enter password is required', async ({ page }) => {
         const errorMessage = 'Re-enter password required';
-        signUpForm.fieldValidation(signUpForm.repeatPasswordField, '', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.repeatPasswordField, '', errorMessage);
     });
 
     test('Re-entered password doesn"t match the Password', async ({ page }) => {
         const errorMessage = 'Passwords do not match';
-        signUpForm.fieldValidation(signUpForm.repeatPasswordField, 'Passwwor1@', errorMessage);
+        await signUpForm.fieldValidation(signUpForm.repeatPasswordField, 'Passwwor1@', errorMessage);
     });
 });
 
@@ -223,7 +223,7 @@ test.describe('Negative scenario with all invalid fields', () => {
     //go to the Register form
     test.beforeEach(async ({ page }) => {
         signUpForm = new SignUpForm(page);
-        signUpForm.open();
+        await signUpForm.open();
     });
 
     test('All fields are invalid', async ({ page }) => {
