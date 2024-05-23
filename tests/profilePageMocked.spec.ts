e6847c915327f23797a8a7945163a29312a671c8
Iryna Blazhenko <iryna.blazhenko@gmail.com>
2024-05-23 13:04:49 +0300

    added test for mocking data

---
+++ tests/profilePageMocked.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+import { GaragePage } from '../page-objects/pages/garagePage';
+
+test.describe('Garage tests with mocking API', () => {
+    let garagePage: GaragePage;
+
+    test.beforeEach(async ({ page }) => {
+        garagePage = new GaragePage(page);
+        await page.goto('/');
+    })
+
+    test('Mock profile name', async ({ page }) => {
+        const resp = {
+            "status": "ok",
+            "data": {
+                "userId": 126015,
+                "photoFilename": "default-user.png",
+                "name": "Joe",
+                "lastName": "Biden"
+            }
+        }
+        await page.route('**/api/users/profile', route => route.fulfill({
+            status: 200,
+            body: JSON.stringify(resp),
+        }));
+        await garagePage.openAsLoggedUser();
+        await page.goto('/panel/profile');
+        await expect(page.locator('[class="profile_name display-4"]')).toHaveText('Joe Biden');
+    });
+});
